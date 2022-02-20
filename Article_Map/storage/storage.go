595eab128b99e6944bad155c613bf8cc9f9e835e
BakhodiribnYashinibnMansur <phapp0224mb@gmail.com>
2022-02-20 21:07:10 +0500

    Article_Map solve Update

--- Article_Map/storage/storage.go
+++ Article_Map/storage/storage.go
@@ -20,6 +20,11 @@ type articleStorage struct {
 var ErrorAlreadyExists = errors.New("already exists id")
 var ErrorNotFound = errors.New("not found id ")
 var ErrorNotUpdated = errors.New("not update because not found id")
+var ErrorSearch = errors.New("not found data")
+var SuccessUpdated = errors.New("SuccessFull Updated")
+var SuccessAdded = errors.New("SuccessFull Added")
+var SuccessDeleted = errors.New("SuccessFull Deleted")
+var SuccessGetByID = errors.New("SuccessFull Got by ID")
 
 //add new article
 func (storage *articleStorage) Add(entity models.Article) error {
@@ -27,7 +32,7 @@ func (storage *articleStorage) Add(entity models.Article) error {
 		return ErrorAlreadyExists
 	}
 	storage.data[entity.ID] = entity
-	return nil
+	return SuccessAdded
 }
 
 //get article by id return map
@@ -35,7 +40,7 @@ func (storage *articleStorage) GetByID(ID int) (models.Article, error) {
 	var resp models.Article
 	resp, ok := storage.data[ID]
 	if ok {
-		return resp, nil
+		return resp, SuccessGetByID
 	}
 	return resp, ErrorNotFound
 }
@@ -64,7 +69,7 @@ func (storage *articleStorage) Search(str string) []models.Article {
 func (storage *articleStorage) Update(entity models.Article) error {
 	if _, ok := storage.data[entity.ID]; ok {
 		storage.data[entity.ID] = entity
-		return nil
+		return SuccessUpdated
 	}
 	return ErrorNotUpdated
 }
@@ -73,7 +78,7 @@ func (storage *articleStorage) Update(entity models.Article) error {
 func (storage *articleStorage) Delete(ID int) error {
 	if _, ok := storage.data[ID]; ok {
 		delete(storage.data, ID)
-		return nil
+		return SuccessDeleted
 	}
 	return ErrorNotFound
 }
