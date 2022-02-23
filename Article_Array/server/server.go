280a44527f65a91f9e117dd3775c1a1b67d11b3b
BakhodiribnYashinibnMansur <phapp0224mb@gmail.com>
2022-02-23 10:16:17 +0500

    add swagger doc

--- Article_Array/server/server.go
+++ Article_Array/server/server.go
@@ -8,10 +8,35 @@ import (
 	"strconv"
 	"time"
 
+	_ "article/docs"
+
 	"github.com/gin-gonic/gin"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
 )
 
 var articleStorage storage.ArticleStorage
+
+// @title           Article API Docs
+// @version         1.1
+// @description     This is a sample article server celler server.
+// @termsOfService  http://github.com/Golang_Tutorial/Article_Array
+// @contact.name   Bakhodir Yashin Mansur
+// @contact.email  phapp0224mb@gmail.com
+// @host      localhost:8080
+
+type DefaultError struct {
+	Message string `json:"message"`
+}
+
+type ResponseError struct {
+	Message string `json:"message"`
+	Code    int    `json:"code"`
+}
+type ResponseSuccess struct {
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
 
 func main() {
 	// initialize database
@@ -42,36 +67,76 @@ func main() {
 	router.GET("/article/id:id", GetArticleByID)
 	router.DELETE("/article/id:id", DeleteArticle)
 	router.PUT("/article/update", UpdateArticle)
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	router.Run(":8080")
 }
 
-//get all article list
+// Show all articles godoc
+// @Summary      Show an all article
+// @Description  get all articles in memory
+// @Tags   Article
+// @ID                get-all-article-handler
+// @Produce      json
+// @Accept        json
+// @Success      200  {array}  models.Article
+// @Failure      500  {string}  DefaultError
+// @Router       /article [GET]
 func GetArticleList(ctx *gin.Context) {
 	resp := articleStorage.GetList()
 	//response body json.
 	ctx.JSON(200, resp)
 }
 
-// create article
+// Create Article godoc
+// @Summary     Create an Article
+// @Description it create article based on on input data
+// @ID create-handler
+// @Tags   Article
+// @Accept       json
+// @Produce      json
+// @Param        data    body  models.Article true "Article data"
+// @Success      200   {object}      ResponseSuccess
+// @Failure      400,404  {object}  ResponseError
+// @Failure      500  {object}  DefaultError
+// @Router       /article [POST]
 func CreateArticle(ctx *gin.Context) {
 	var article models.Article
 	//read request json data.
 	err := ctx.BindJSON(&article)
 	if err != nil {
 		log.Println(err)
-		ctx.JSON(422, err.Error())
+		ctx.JSON(400, ResponseError{
+			Message: err.Error(),
+			Code:    400,
+		})
 		return
 	}
 	err = articleStorage.Add(article)
 	if err != nil {
 		log.Println(err)
-		ctx.JSON(400, err.Error())
+		ctx.JSON(400, ResponseError{
+			Message: err.Error(),
+			Code:    400,
+		})
 		return
 	}
-	ctx.JSON(200, "message : Success Created Article")
+	ctx.JSON(200, ResponseSuccess{
+		Message: "Success Created Article",
+	})
 }
 
-// search article data via article data
+// Search article via title  godoc
+// @Summary    search article via title
+// @Description it search on base via response title
+// @ID search-handler
+// @Tags   Article
+// @Accept       json
+// @Produce      json
+// @Param        query    query  string true "Query Title"
+// @Success      200   {array}      models.Article
+// @Failure      400,404  {object}  ResponseError
+// @Failure      500  {object}  DefaultError
+// @Router       /article/search [GET]
 func SearchArticle(ctx *gin.Context) {
 	//read router query e.x. {{base_url}}article/search?query=Lorem
 	searchText := ctx.Query("query")
@@ -79,71 +144,134 @@ func SearchArticle(ctx *gin.Context) {
 		searchedList := articleStorage.Search(searchText)
 		if len(searchedList) == 0 {
 			//response json data gin.H
-			ctx.JSON(400, gin.H{"message": " no result search ", "data": searchedList})
+			ctx.JSON(400, ResponseError{
+				Message: "None Result Data", Code: 400,
+			})
 			return
 		} else {
 			ctx.JSON(200, searchedList)
 			return
 		}
 	} else {
-		ctx.JSON(400, "bad input")
+		ctx.JSON(400, ResponseError{
+			Message: "Bad Input",
+			Code:    400,
+		})
 	}
 }
 
-//get article by id
+// Get Article by Id  godoc
+// @Summary   get article by id
+// @Description it return Article which you send id
+// @ID getarticle-by-id-handler
+// @Tags   Article
+// @Accept       json
+// @Produce      json
+// @Param        id   path  int     true "Param ID"
+// @Success      200   {object}      models.Article
+// @Failure      400,404  {object}  ResponseError
+// @Failure      500  {object}  DefaultError
+// @Router       /article/id{id} [GET]
 func GetArticleByID(ctx *gin.Context) {
 	//read router param e.x. {{base_url}}article/id7
 	paramID := ctx.Param("id")
 	if paramID != "" {
 		id, err := strconv.Atoi(paramID)
 		if err != nil {
-			ctx.JSON(400, gin.H{"message": err.Error()})
+			ctx.JSON(400, ResponseError{
+				Message: err.Error(),
+				Code:    400,
+			})
 			log.Println(err)
 			return
 		}
 		getArticle, err := articleStorage.GetByID(id)
 		if err != nil {
-			ctx.JSON(400, gin.H{"message": err.Error()})
+			ctx.JSON(400, ResponseError{
+				Message: err.Error(),
+				Code:    400,
+			})
 			log.Println(err)
 			return
 		}
-		ctx.JSON(200, gin.H{"data": getArticle})
+		ctx.JSON(200, ResponseSuccess{
+			Message: "Successfull",
+			Data:    getArticle,
+		})
 	}
 }
 
-//delete Article
+// Delete Article by Id  godoc
+// @Summary   delete article by id
+// @Description it delete Article which you send id of article
+// @ID delete-handler
+// @Tags   Article
+// @Accept       json
+// @Produce      json
+// @Param        id   path  int     true "Param ID"
+// @Success      200   {object}    ResponseSuccess
+// @Failure      400,404  {object}  ResponseError
+// @Failure      500  {object}  DefaultError
+// @Router       /article/id{id} [DELETE]
 func DeleteArticle(ctx *gin.Context) {
 	paramID := ctx.Param("id")
 	if paramID != "" {
 		id, err := strconv.Atoi(paramID)
 		if err != nil {
-			ctx.JSON(400, gin.H{"message": err.Error()})
+			ctx.JSON(400, ResponseError{
+				Message: err.Error(),
+				Code:    400,
+			})
 			log.Println(err)
 			return
 		}
 		err = articleStorage.Delete(id)
 		if err != nil {
-			ctx.JSON(200, gin.H{"message": err.Error()})
+			ctx.JSON(400, ResponseError{
+				Message: err.Error(),
+				Code:    400,
+			})
 			return
 		}
-		ctx.JSON(200, gin.H{"message": "success deleted"})
+		ctx.JSON(200, ResponseSuccess{
+			Message: "Successfull Deleted",
+		})
 	}
 }
 
-//update article via data
+// Update Article godoc
+// @Summary     Update an Article
+// @Description it update article based on on input data
+// @ID update-handler
+// @Tags   Article
+// @Accept       json
+// @Produce      json
+// @Param        data    body  models.Article true "Article data"
+// @Success      200   {object}      ResponseSuccess
+// @Failure      400,404  {object}  ResponseError
+// @Failure      500  {object}  DefaultError
+// @Router       /article/update [PUT]
 func UpdateArticle(ctx *gin.Context) {
 	var article models.Article
 	err := ctx.BindJSON(&article)
 	if err != nil {
 		log.Println(err)
-		ctx.JSON(422, err.Error())
+		ctx.JSON(422, ResponseError{
+			Message: err.Error(),
+			Code:    422,
+		})
 		return
 	}
 	err = articleStorage.Update(article)
 	if err != nil {
 		log.Println(err)
-		ctx.JSON(400, err.Error())
+		ctx.JSON(400, ResponseError{
+			Message: err.Error(),
+			Code:    400,
+		})
 		return
 	}
-	ctx.JSON(200, "message : Success Created Article")
+	ctx.JSON(200, ResponseSuccess{
+		Message: "Successfull Updated",
+	})
 }
