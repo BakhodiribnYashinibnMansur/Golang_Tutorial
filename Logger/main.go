659099386d4f47cd8ffb9a192fe0fedb91e4a79b
BakhodiribnYashinibnMansur <phapp0224mb@gmail.com>
2022-03-18 23:05:32 +0500

    updated logrus

--- Logger/main.go
+++ Logger/main.go
@@ -7,8 +7,14 @@ import (
 func main() {
 
 	logger := log.GetLogger()
-	logger.Info("Info")
-	// logger.Fatal(errors.New("Error"))
-	logger.Debug("Debug")
-	// logger.Error("Error")
+
+	logger.Trace("Something very low level.")
+	logger.Debug("Useful debugging information.")
+	logger.Info("Something noteworthy happened!")
+	logger.Warn("You should probably take a look at this.")
+	logger.Error("Something failed but I'm not quitting.")
+	// Calls os.Exit(1) after loggerging
+	logger.Fatal("Bye.")
+	// Calls panic() after loggerging
+	logger.Panic("I'm bailing.")
 }
